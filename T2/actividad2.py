749646519ebc2f2118f3381458363c6bba229fd8
SAMERU <samuelumbariba@gmail.com>
2025-09-04 10:57:05 -0500

    Update actividad2.py

--- T2/actividad2.py
+++ T2/actividad2.py
@@ -1,4 +1,4 @@
-# •	Tabla de Multiplicar: Crea un programa que solicite al usuario un número
+# • actividad  :	 	Tabla de Multiplicar: Crea un programa que solicite al usuario un número
 # y luego imprima la tabla de multiplicar de ese número del 1 al 10.
 
 def tablaMultiplicar (numero):
@@ -9,6 +9,7 @@ def tablaMultiplicar (numero):
 def pedirNumero():
     numero = input("ingrese un numero para ver su tabla de multiplicar: ")       
     return int(numero)
+
 
 
 print(tablaMultiplicar(pedirNumero()))
